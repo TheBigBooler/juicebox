ebc928e9720c7d977007ede73297dd6e2cf65402
James <>
2023-07-05 19:31:13 -0500

    working on getUserById function

--- db/index.js
+++ db/index.js
@@ -1,32 +1,127 @@
-const Pool = require('pg').Pool
+const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: 'me',
-    host: "localhost",
-    database: "juiceboxdev",
-    password: "Platypus123!",
-    port: 5432
-})
+  user: "me",
+  host: "localhost",
+  database: "juiceboxdev",
+  password: "Platypus123!",
+  port: 5432,
+});
 
 const getAllUsers = async () => {
-    const { rows } = await pool.query(
-        `SELECT id, username
+  const { rows } = await pool.query(
+    `SELECT id, username, name, location, active
         FROM users;`
+  );
+  return rows;
+};
+
+const createUser = async ({ username, password, name, location }) => {
+  try {
+    const { rows: [ user ] } = await pool.query(
+      `
+            INSERT INTO users (username, password, name, location) VALUES ($1, $2, $3, $4) ON CONFLICT (username) DO NOTHING RETURNING *;
+        `,
+      [username, password, name, location]
     );
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+async function updateUser(id, fields = {}) {
+  // build the set string
+  const setString = Object.keys(fields).map(
+    (key, index) => `"${ key }"=$${ index + 1 }`
+  ).join(', ');
+
+  // return early if this is called without fields
+  if (setString.length === 0) {
+    return;
+  }
+
+  try {
+    const { rows: [ user ] } = await pool.query(`
+      UPDATE users
+      SET ${ setString }
+      WHERE id=${id}
+      RETURNING *;
+    `, Object.values(fields));
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+const createPost = async ({ authorId, title, content }) => {
+  try {
+    const { rows } = await pool.query(
+    `INSERT INTO posts ("authorId", title, content) VALUES ($1, $2, $3) RETURNING *;`,
+    [authorId, title, content]);
+
     return rows;
+  } catch (error) {
+    throw error; 
+  }
 }
 
-const createUser = async ({username, password}) => {
-    try {
-        const { rows } = await pool.query(`
-            INSERT INTO users (username, password) VALUES ($1, $2) ON CONFLICT (username) DO NOTHING RETURNING *;
-        `, [ username, password ]);
+async function updatePost(id, fields = {}) {
+  
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(', ');
 
-        return rows;
-    } catch (error) {
-        throw error;
-    }
+  if (setString.length === 0) {
+    return;
+  }
+
+  try {
+    const {
+      rows: [post],
+    } = await pool.query(
+      `
+      UPDATE posts
+      SET ${setString}
+      WHERE id=${id}
+      RETURNING *;
+    `,
+      Object.values(fields)
+    );
+
+    return post;
+  } catch (error) {
+    throw error;
+  }
+}
+
+const getAllPosts = async () => {
+  const { rows } = await pool.query(
+    `SELECT "authorId", title, content
+        FROM posts;`
+  );
+  return rows;
+};
+
+const getPostsByUser = async (userId) => {
+  try {
+    const { rows } = await pool.query(`
+    SELECT * FROM posts
+    WHERE "authorId"=$1;
+    `, [userId]);
+
+    return rows;
+
+  } catch (error) {
+    throw error;
+  }
+}
+
+const getUserById = async (userId) => {
+  
 }
 
 
-module.exports = { pool, getAllUsers, createUser }
+module.exports = { pool, getAllUsers, createUser, updateUser, createPost, updatePost, getAllPosts, getPostsByUser };
--- db/seed.js
+++ db/seed.js
@@ -1,14 +1,21 @@
-const { pool, getAllUsers, createUser } = require('./index');
+const { pool, getAllUsers, createUser, updateUser } = require('./index');
 
 const testDB = async () => {
     try {
-        console.log('Starting to test database...')
+        console.log('Starting to test database...');
         const users = await getAllUsers();
         console.log("getAllUsers:", users);
 
-        console.log("Finished database test")
+        console.log("Calling updateUser on users[0]");
+        const updateUserResult = await updateUser(users[0].id, {
+            name: "Newname Sogood",
+            location: "Lesterville, KY"
+        });
+        console.log("Update user:", updateUserResult);
+
+        console.log("Finished database test");
     } catch (error) {
-        console.error("Error testing database")
+        console.error("Error testing database");
         throw error;
     }
 }
@@ -19,19 +26,26 @@ const createInitialUsers = async () => {
 
         const albert = await createUser({
             username: 'albert', 
-            password: 'bertie99'});
+            password: 'bertie99',
+            name: 'Albert',
+            location: "California"
+        });
         const sandra = await createUser({
           username: "sandra",
           password: "2sandy4me",
+          name: "Sandra Bullock",
+          location: "Alaska"
         });
         const glamgal = await createUser({
           username: "glamgal",
           password: "soglam",
+          name: "Fergie",
+          location: "Hollywood"
         });
 
-        console.log(albert);
-        console.log(sandra);
-        console.log(glamgal);
+        // console.log(albert);
+        // console.log(sandra);
+        // console.log(glamgal);
 
         console.log("Finished creating users!")
     } catch (error) {
@@ -45,6 +59,7 @@ const dropTables = async () => {
         console.log("Dropping tables...");
 
         await pool.query(`
+        DROP TABLE IF EXISTS posts;
         DROP TABLE IF EXISTS users;
         `);
 
@@ -62,9 +77,22 @@ const createTables = async () => {
         CREATE TABLE users (
         id SERIAL PRIMARY KEY,
         username varchar(255) UNIQUE NOT NULL,
-        password varchar(255) NOT NULL
+        password varchar(255) NOT NULL,
+        name varchar(255) NOT NULL,
+        location varchar(255) NOT NULL,
+        active BOOLEAN DEFAULT true
       );
         `);
+    await pool.query(`
+        CREATE TABLE posts (
+            id SERIAL PRIMARY KEY,
+            "authorId" INTEGER REFERENCES users(id) NOT NULL,
+            title VARCHAR(255) NOT NULL,
+            content TEXT NOT NULL,
+            active BOOLEAN DEFAULT true
+        );
+        `);        
+
         console.log("Finished building tables!");
   } catch (error) {
       console.error("Error building tables");
