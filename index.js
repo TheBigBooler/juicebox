cf52ca334f665ea30f01e8189f92db192c99b4f0
James <>
2023-07-07 15:51:33 -0500

    added morgan and basic users route

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-const PORT = 3000; 
-const express = require('express');
+const PORT = 3000;
+const express = require("express");
 const server = express();
+const morgan = require("morgan");
 
+server.use(morgan("dev"));
+server.use(express.json());
 
+// Logging functionality
 server.use((req, res, next) => {
-    console.log("<---Body Logger START--->");
-    console.log(req.body);
-    console.log("<---Body Logger END--->")
+  console.log("<---Body Logger START--->");
+  console.log(req.body);
+  console.log("<---Body Logger END--->");
 
-    next();
-})
+  next();
+});
+
+const apiRouter = require("./api");
+server.use("/api", apiRouter);
+
 
 server.listen(PORT, () => {
-    console.log("Server is running on port", PORT)
+  console.log("Server is running on port", PORT);
 });
